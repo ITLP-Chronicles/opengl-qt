b7fb1429698598739b13d2d89927a4b4cdb414e2
Raul Mendez <raulmendezpersonal.230@gmail.com>
2024-11-26 20:22:56 -0700

    Correct rotation

--- openglwidget.cpp
+++ openglwidget.cpp
@@ -10,32 +10,32 @@ void OpenGLWidget::initializeGL() {
     mouseY = 1;
 
     glEnable(GL_COLOR_MATERIAL);
-    glClearColor(1,1,1,0);
+    glClearColor(1, 1, 1, 0);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    timer.start(50,this);
+    timer.start(50, this);
 }
 
-void OpenGLWidget::timerEvent(QTimerEvent *){
-    angulo++;
+void OpenGLWidget::timerEvent(QTimerEvent *) {
+    angulo += sentido;
     update();
 }
 
-void OpenGLWidget::mousePressEvent(QMouseEvent *e){
+void OpenGLWidget::mousePressEvent(QMouseEvent *e) {
     timer.start(50, this);
-    if(e->button() == Qt::RightButton){
+    if (e->button() == Qt::RightButton) {
         sentido = 1;
-    }
-    else if(e->button()==Qt::LeftButton){
+    } else if (e->button() == Qt::LeftButton) {
         sentido = -1;
     }
-    //angulo += sentido;
 }
-void OpenGLWidget::mouseReleaseEvent(QMouseEvent *e){
+
+void OpenGLWidget::mouseReleaseEvent(QMouseEvent *e) {
     timer.stop();
 }
-void OpenGLWidget::mouseMoveEvent(QMouseEvent *e){
+
+void OpenGLWidget::mouseMoveEvent(QMouseEvent *e) {
     int ancho = width();
     int alto = height();
 
@@ -68,13 +68,11 @@ void OpenGLWidget::paintGL() {
 
     glTranslatef(0.25, 0.25, -0.25);
     glRotatef(10, 1.0, 0, 0);
-    glRotatef(sentido*angulo, 0, 1.0, 0);
+    glRotatef(angulo, 0, 1.0, 0);
     glTranslatef(-0.25, -0.25, 0.25);
 
     glBegin(GL_QUADS);
 
-
-    //Blanco
     glNormal3f(0, 0, 1);
     glColor3f(1, 1, 1);
     glVertex3f(0.0, 0.0, 0.0);
@@ -82,7 +80,6 @@ void OpenGLWidget::paintGL() {
     glVertex3f(0.5, 0.5, 0.0);
     glVertex3f(0.0, 0.5, 0.0);
 
-    //Rojo
     glNormal3f(0, 0, -1);
     glColor3f(1, 0, 0);
     glVertex3f(0.0, 0.0, -0.5);
@@ -90,7 +87,6 @@ void OpenGLWidget::paintGL() {
     glVertex3f(0.5, 0.5, -0.5);
     glVertex3f(0.5, 0.0, -0.5);
 
-    //Azul
     glNormal3f(1, 0, 0);
     glColor3f(0, 0, 1);
     glVertex3f(0.5, 0.0, 0.0);
@@ -98,7 +94,6 @@ void OpenGLWidget::paintGL() {
     glVertex3f(0.5, 0.5, -0.5);
     glVertex3f(0.5, 0.5, 0.0);
 
-    //Naranja
     glNormal3f(-1, 0, 0);
     glColor3f(1, 0.5, 0.0);
     glVertex3f(0.0, 0.0, 0.0);
@@ -106,7 +101,6 @@ void OpenGLWidget::paintGL() {
     glVertex3f(0.0, 0.5, -0.5);
     glVertex3f(0.0, 0.0, -0.5);
 
-    //Verde
     glNormal3f(0, 1, 0);
     glColor3f(0, 1, 0);
     glVertex3f(0.0, 0.5, 0.0);
@@ -114,7 +108,6 @@ void OpenGLWidget::paintGL() {
     glVertex3f(0.5, 0.5, -0.5);
     glVertex3f(0.0, 0.5, -0.5);
 
-    //Amarillo
     glNormal3f(0, -1, 0);
     glColor3f(1, 1, 0);
     glVertex3f(0.0, 0.0, 0.0);
