ee1ad603cab3e4b2f27bc4ae7142d55ffb6a8f67
Kristan <itan_07@hotmail.com>
2024-11-27 09:14:55 -0600

    Krssss

--- openglwidget.cpp
+++ openglwidget.cpp
@@ -3,72 +3,109 @@
 OpenGLWidget::OpenGLWidget() {}
 
 void OpenGLWidget::initializeGL() {
-    glClearColor(1,1,1,0);
+    glClearColor(0,0,0,0);
+    anguloAgregar = 1;
+    timer.start(50, this);
 }
 
 void OpenGLWidget::paintGL() {
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_COLOR_MATERIAL);
     glLoadIdentity();
 
-    glRotatef(10, 1.0, 1.0, 0);
-    // Cara roja
-    glColor3f(1.0, 0.0, 0.0);
-    glBegin(GL_POLYGON);
-    glVertex3f(-0.5, -0.5, 0.5);
-    glVertex3f(0.5, -0.5, 0.5);
-    glVertex3f(0.5, 0.5, 0.5);
-    glVertex3f(-0.5, 0.5, 0.5);
-    glEnd();
+    float luzAmbiente[] = {0.2, 0.2, 0.2, 1};
+    float luzDifusa[] = {1.0, 1.0, 1.0,1};
+    float luzPosicion[] = {0, 1, 1, 1};
 
-    // Cara verde
-    glColor3f(0.0, 1.0, 0.0);
-    glBegin(GL_POLYGON);
-    glVertex3f(-0.5, -0.5, -0.5);
-    glVertex3f(0.5, -0.5, -0.5);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, luzAmbiente);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, luzDifusa);
+    glLightfv(GL_LIGHT0, GL_POSITION, luzPosicion);
+
+    glRotated(anguloY, 0, 1 ,0);
+    glRotated(15, 1, 0 ,0);
+    glTranslatef(-0.25, -0.25, 0.25);
+    glColor3f(1, 0, 0);
+    glBegin(GL_QUADS);
+
+    //Blanco
+    glNormal3f(0, 0, 1);
+    //glColor3f(1, 1, 1);
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(0.5, 0.0, 0.0);
+    glVertex3f(0.5, 0.5, 0.0);
+    glVertex3f(0.0, 0.5, 0.0);
+
+    //Rojo
+    glNormal3f(0, 0, -1);
+    //glColor3f(1, 0, 0);
+    glVertex3f(0.0, 0.0, -0.5);
+    glVertex3f(0.0, 0.5, -0.5);
     glVertex3f(0.5, 0.5, -0.5);
-    glVertex3f(-0.5, 0.5, -0.5);
-    glEnd();
+    glVertex3f(0.5, 0.0, -0.5);
 
-    // Cara azul
-    glColor3f(0.0, 0.0, 1.0);
-    glBegin(GL_POLYGON);
-    glVertex3f(-0.5, 0.5, -0.5);
+    //Azul
+    glNormal3f(1, 0, 0);
+    //glColor3f(0, 0, 1);
+    glVertex3f(0.5, 0.0, 0.0);
+    glVertex3f(0.5, 0.0, -0.5);
     glVertex3f(0.5, 0.5, -0.5);
-    glVertex3f(0.5, 0.5, 0.5);
-    glVertex3f(-0.5, 0.5, 0.5);
-    glEnd();
+    glVertex3f(0.5, 0.5, 0.0);
 
-    // Cara amarilla
-    glColor3f(1.0, 1.0, 0.0);
-    glBegin(GL_POLYGON);
-    glVertex3f(-0.5, -0.5, -0.5);
-    glVertex3f(0.5, -0.5, -0.5);
-    glVertex3f(0.5, -0.5, 0.5);
-    glVertex3f(-0.5, -0.5, 0.5);
-    glEnd();
+    //Naranja
+    glNormal3f(-1, 0, 0);
+    //glColor3f(1, 0.5, 0.0);
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(0.0, 0.5, 0.0);
+    glVertex3f(0.0, 0.5, -0.5);
+    glVertex3f(0.0, 0.0, -0.5);
 
-    // Cara naranja
-    glColor3f(1.0, 0.5, 0.0);
-    glBegin(GL_POLYGON);
-    glVertex3f(0.5, -0.5, -0.5);
+    //Verde
+    glNormal3f(0, 1, 0);
+    //glColor3f(0, 1, 0);
+    glVertex3f(0.0, 0.5, 0.0);
+    glVertex3f(0.5, 0.5, 0.0);
     glVertex3f(0.5, 0.5, -0.5);
-    glVertex3f(0.5, 0.5, 0.5);
-    glVertex3f(0.5, -0.5, 0.5);
+    glVertex3f(0.0, 0.5, -0.5);
+
+    //Amarillo
+    glNormal3f(0, -1, 0);
+    //glColor3f(1, 1, 0);
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(0.0, 0.0, -0.5);
+    glVertex3f(0.5, 0.0, -0.5);
+    glVertex3f(0.5, 0.0, 0.0);
+
     glEnd();
+}
 
-    // Cara blanca
-    glColor3f(0.9, 0.9, 0.9);
-    glBegin(GL_POLYGON);
-    glVertex3f(-0.5, -0.5, -0.5);
-    glVertex3f(-0.5, 0.5, -0.5);
-    glVertex3f(-0.5, 0.5, 0.5);
-    glVertex3f(-0.5, -0.5, 0.5);
-    glEnd();
 
-    /*
-    glTranslatef(0.25,0.25,0.0);
-    glScalef(-0.5,-0.5,0);
-    glRotatef(45, 0.0, 0.0, 1.0);
-    glTranslatef(-0.25,-0.25,0.0);
-    */
+void OpenGLWidget::mouseMoveEvent(QMouseEvent *_){
+    update();
+}
 
+
+void OpenGLWidget::mousePressEvent(QMouseEvent *event){
+    timer.start(50, this);
+
+    if (event->button() == Qt::RightButton)
+        anguloAgregar = 1;
+    else
+        anguloAgregar = -1;
+
+    update();
+}
+
+
+void OpenGLWidget::mouseReleaseEvent(QMouseEvent *_){
+    timer.stop();
+}
+
+
+void OpenGLWidget::timerEvent(QTimerEvent *_){
+    anguloY += anguloAgregar;
+    update();
 }
--- openglwidget.h
+++ openglwidget.h
@@ -1,15 +1,24 @@
 #ifndef OPENGLWIDGET_H
 #define OPENGLWIDGET_H
 
+#include <QBasicTimer>
 #include <QOpenGLWidget>
+#include <QMouseEvent>
 
 class OpenGLWidget : public QOpenGLWidget
 {
 public:
     OpenGLWidget();
 private:
+    int anguloAgregar;
+    int anguloY;
+    QBasicTimer timer;
     void initializeGL();
     void paintGL();
+    void mouseMoveEvent(QMouseEvent *);
+    void mousePressEvent(QMouseEvent *);
+    void mouseReleaseEvent(QMouseEvent *);
+    void timerEvent(QTimerEvent *);
 };
 
 #endif // OPENGLWIDGET_H
